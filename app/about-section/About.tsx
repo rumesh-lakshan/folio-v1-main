14e096f2d1f76c67bb9e9c59c1c5e2741b23efa9
PeirisWRL <123114138+rumesh-lakshan@users.noreply.github.com>
2023-11-25 22:06:27 +0530

    edit about

--- app/about-section/About.tsx
+++ app/about-section/About.tsx
@@ -1,4 +1,4 @@
-import SongCarousel from "./SongCarousel";
+
 import "../animations/animate.css";
 import AnimatedBody from "../animations/AnimatedBody";
 import AnimatedTitle from "../animations/AnimatedTitle";
@@ -12,7 +12,7 @@ const About = () => {
       <div className="mx-auto flex w-[90%] flex-col items-center justify-center lg:max-w-[1212.8px]">
         <AnimatedTitle
           text={
-            "SMT ABOUT"
+            "ABOUT"
           }
           className={
             "mb-10 text-left text-[40px] font-bold leading-[0.9em] tracking-tighter text-[#e4ded7] sm:text-[45px] md:mb-16 md:text-[60px] lg:text-[80px]"
@@ -25,7 +25,7 @@ const About = () => {
           <div className="mb-10 flex w-[100%] flex-col gap-4 text-[18px] font-medium  leading-relaxed tracking-wide text-[#e4ded7] md:mb-16 md:gap-6 md:text-[20px] md:leading-relaxed lg:mb-16  lg:max-w-[90%] lg:text-[24px] ">
             <AnimatedBody
               text={
-                "My passion lies in creating software that solves real-world problems and positively impacts people’s lives."
+                "SMT TRAVELS AND TOURS (PVT) LIMITED, is a licensed recruitment company under the Sri Lanka Bureau of Foreign Employment as (License No: 3118)."
               }
             />
             <AnimatedBody
@@ -36,75 +36,22 @@ const About = () => {
             />
             <AnimatedBody
               text={
-                "In addition to my work in software development, I’m also an active leader and member of tech communities on campus, including the Google Developer Student Clubs and Microsoft Learn Student Ambassadors."
+                "SMT Travels and Tours has committed professional teams with industry-specific specializations to guarantee that the proper expertise is sourced to meet the needs of our clients. Every team is led by a seasoned expert with extensive experience. We source applicants for our multinational clients, the majority of whom are stationed in Romania and the Middle East.  "
               }
             />
             <AnimatedBody
               text={
-                "Whether it’s binge-watching my favorite anime series, cheering on Manchester United, hanging out with friends, or discovering new music in my favorite genres like RnB, UK Drill, and Chill Rap, I’m always looking for ways to relax and unwind."
+                "We work closely with our clients to assist them in becoming high-performance establishments by conducting in-depth research on labor market trends across various geographic areas. We view every hiring project as distinct."
               }
             />
             <AnimatedBody
               text={
-                "Right now, I’m working on some exciting projects that I can’t wait to share with you. But I’m always open to new opportunities and collaborations."
+                " Our core values—Respect, Diversity, Empowerment, Teamwork, and Community—allow us to concentrate on a number of areas that will contribute to the advancement of society as a whole as well as ourselves."
               }
             />
           </div>
 
-          <div className="mb-24 flex w-[100%] flex-col gap-4 text-[18px] font-normal leading-relaxed tracking-wide text-[#e4ded7]/80 sm:mb-32 md:mb-40 md:gap-6 md:text-[16px] md:leading-normal lg:mt-0 lg:mb-16 lg:max-w-[30%] lg:text-[18px]">
-            <div className="flex flex-col gap-4 md:gap-3">
-              <AnimatedTitle
-                text={"Frontend Tools"}
-                className={
-                  "text-[24px] text-[#e4ded7] md:text-[30px] lg:text-[20px]"
-                }
-                wordSpace={"mr-[0.25em]"}
-                charSpace={"mr-[0.01em]"}
-              />
-              <AnimatedBody
-                text={
-                  "JavaScript(ES6+), React, Next.js, TypeScript, Redux, Redux Toolkit, React Testing Library, Vitetest, HTML5, Git/GitHub, NextAuth, Formik."
-                }
-              />
-            </div>
-            <div className="flex flex-col gap-3">
-              <AnimatedTitle
-                text={"UI Libraries"}
-                className={
-                  "text-[24px] text-[#e4ded7] md:text-[30px] lg:text-[20px]"
-                }
-                wordSpace={"mr-[0.25em]"}
-                charSpace={"mr-[0.01em]"}
-              />
-              <AnimatedBody
-                text={
-                  "CSS3/SCSS/SASS, Tailwind CSS, Material UI, Framer Motion, GSAP, Bootstrap, Chart.js."
-                }
-              />
-            </div>
-            <div className="flex flex-col gap-3">
-              <AnimatedTitle
-                text={"Design Tools"}
-                className={
-                  "text-[24px] text-[#e4ded7] md:text-[30px] lg:text-[20px]"
-                }
-                wordSpace={"mr-[0.25em]"}
-                charSpace={"mr-[0.01em]"}
-              />
-              <AnimatedBody
-                text={
-                  "Figma, FigJam, Adobe XD, ProtoPie, Adobe Photoshop, UX Research, UI Design, Prototyping."
-                }
-              />
-            </div>
-          </div>
-        </div>
-        <div className="mt-10 flex flex-col md:-mt-0 lg:mt-28">
-          <SongCarousel />
-          <AnimatedBody
-            text="A few songs I can recommend if you're looking for some fresh tunes :)"
-            className="absolute bottom-10 right-0 left-0 mx-auto w-[90%] text-center text-[14px] font-semibold uppercase text-[#e4ded7] sm:w-[500px] md:bottom-12 md:w-[550px] md:text-[16px] "
-          />
+         
         </div>
       </div>
     </section>
